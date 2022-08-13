4cefdcfdf9dd8c9939108522c930c6dd54f19bf9
PJJ <puayjianjun@gmail.com>
2022-08-13 22:47:23 +0800

    Completed Exercises 3.19-3.21

--- models/person.js
+++ models/person.js
@@ -11,9 +11,32 @@ mongoose.connect(url)
         console.log('error connecting to MongoDB: ', error.message)
     })
 
+const numValidator = (number) => {
+    let count = (number.match(/-/g) || []).length
+    console.log(count)
+    if (count === 0) {
+        return true
+    } else if (count === 1) {
+        let idx = number.indexOf('-')
+        return idx === 2 || idx === 3
+    } else {
+        return false
+    }
+    // return count === 0 || count === 1
+}
+
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        minLength: 8,
+        // validate: [numberValidator, `phone number is shorter than 8`]
+        validate: [numValidator, 'invalid phone number type']
+    }
   })
 
 
